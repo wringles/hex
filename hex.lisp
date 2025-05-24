ab1aca8a0c54385be74653a629f2a11666dacbb1
Nelson Kim <dead_mohican@yahoo.com>
2025-05-24 11:31:59 -0300

    added definition of hex class

--- hex.lisp
+++ hex.lisp
@@ -1,3 +1,15 @@
+;;; Hexcrawler package
+;;;
+;;; by definition, hexes are aligned horizontally
+;;; hex 0101 is on the top left of the map, and is leftmost
+;;; (therefore hex 0201 is below and to the right of 0101)
+;;; (also, even numbered rows are always biased to the right)
+
+(defpackage hexcrawler
+  (:use :cl))
+
+(in-package hexcrawler)
+
 (defgeneric test-which-method (object)
   (:method (object)
      (list "This is an object."))
@@ -15,4 +27,37 @@
    (tag :initform (gensym) :reader read-tag)
    (value :accessor value-of)))
 
-(defclass)
+
+(defclass hex ()
+    ((x :initarg :x
+        :accessor x)
+     (y :initarg :y
+        :accessor y)
+     (terrain :initarg :terrain
+              :initform 'plain
+              :accessor terrain)
+     (climate :initarg :climate
+              :initform 'temperate
+              :accessor climate)
+     (description :initarg :description
+                  :accessor description)
+     (features :accessor features)
+     (scale :initarg :scale
+            :initform 'midi  ;;; maxi 30mi, midi 5mi
+            :reader scale)))
+
+;;; climate types:
+;;;   Arctic
+;;;   Sub-arctic
+;;;   Temperate
+;;;   Sub-tropical
+;;;   Tropical
+
+;;; terrain types:
+;;;   Water
+;;;   Swamp
+;;;   Desert
+;;;   Plains
+;;;   Forest
+;;;   Hills
+;;;   Mountains
